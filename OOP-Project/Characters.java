6450b10bfe01b17da5dafeaf12dc94e0caa06b58
vihanga_02 <muthumalavdw.22@uom.lk>
2024-03-06 15:42:19 +0530

    Character class created

--- OOP-Project/Characters.java
+++ OOP-Project/Characters.java
@@ -1,2 +1,42 @@
 public class Characters {
+    private String name;
+    private double price;
+    private double attac;
+    private double defence;
+    private double health;
+    private double speed;
+
+    public Characters(String name, double price, double attac, double defence, double health, double speed){
+        this.name = name;
+        this.price = price;
+        this.attac = attac;
+        this.defence = defence;
+        this.health = health;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public double getAttac() {
+        return attac;
+    }
+    public double getDefence() {
+        return defence;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
 }
