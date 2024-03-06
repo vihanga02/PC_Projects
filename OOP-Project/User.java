6375130715cdafcf36bd550adcd6d30c755eee13
vihanga_02 <muthumalavdw.22@uom.lk>
2024-03-06 16:06:43 +0530

    Basic user class created

---
+++ OOP-Project/User.java
@@ -0,0 +1,47 @@
+public class User {
+    private String name;
+    private String userName;
+    private double coins;
+    private int id;
+    private double xp;
+
+    public User(String name, String userName, int id, double xp){
+        this.name = name;
+        this.userName = userName;
+        this.coins = 500;
+        this.id = id;
+        this.xp = xp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public double getCoins() {
+        return coins;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getXp() {
+        return xp;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCoins(double coins) {
+        this.coins = coins;
+    }
+
+    public void setXp(double xp) {
+        this.xp = xp;
+    }
+}
