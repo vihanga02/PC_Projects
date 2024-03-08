a62b568d202dac6ba1e9185a3b271b4af80d3962
Rivindu Thareendra <ugrtk2002@gmail.com>
2024-03-08 13:27:12 +0530

    print ek

--- OOP-Project/Main.java
+++ OOP-Project/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
         Character archer = new Archer("Sunfire");
         System.out.println("Archer type Sunfire's Health: " + archer.getHealth());
+        System.out.println("Print PAKAYA");
 
     }
 }
