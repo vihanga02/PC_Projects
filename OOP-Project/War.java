11ffd1a1a2d0816eff6bbe4932f995150aa9ae1c
trinith01 <gt202054104@gmail.com>
2024-03-08 16:44:33 +0530

    Edited War Class

--- OOP-Project/War.java
+++ OOP-Project/War.java
@@ -1,20 +1,77 @@
+import java.util.Vector;
+import java.util.Comparator;
+
 public class War {
-    User intialChallenger;
-    User intialDefender;
+    User initialChallenger;
+    User initialOpponent;
     User challenger;
-    User defender;
-    public War(User intialChallenger,User intialDefender){
-        this.intialChallenger = intialChallenger;
-        this.intialDefender = intialDefender;
+    User opponent;
+    Vector<Character> challengerArmy;
+    Vector<Character> opponentArmy;
+    boolean isWarEnded=false;
+
+
+3
+    public War(User initialChallenger,User initialOpponent){
+        this.initialChallenger = initialChallenger;
+        this.initialOpponent = initialOpponent;
 
 
 
     }
     void startWar(){
+
         this.challenger = intialChallenger;
-        this.defender = intialDefender;
-        String homeGround = challenger.homeGround;
-        if(homeGround=="Hillcrest")
+        this.opponent = intialOpponent;
+        this.challengerArmy = challenger.getArmy();
+        this.defenderArmy= opponent.getArmy();
+        for(Character character:challengerArmy){
+            if(challenger.homeGround.equals("hIllCrest")) {
+                if (character.characterType.equals("highLander")) {
+                    character.setAttack(character.getAttack() + 1);
+                    character.setDefence(character.getDefence() + 1);
+                }
+            }else if(challenger.homeGround.equals("marshLand")) {
+                if (character.characterType.equals("marshLander")) {
+                    character.setDefence(character.getDefence() + 2);
+                }
+                if (character.characterType.equals("sunChild")) {
+                    character.setAttack(character.getAttack() - 1);
+
+                }
+                if (character.characterType.equals("mystics")) {
+                    character.setSpeed(character.getSpeed() - 1);
+
+                }
+            }else if(challenger.homeGround.equals("desert")){
+                    if(character.characterType.equals("marshLander")){
+                        character.setHealth(character.getHealth() - 1);
+                    }if(character.characterType.equals("sunChild")){
+                        character.setAttack(character.getAttack() + 1);
+
+                    }
+
+            }else(challenger.homeGround.equals("arcane")){
+                if(character.characterType.equals("mystics")){
+                    character.setAttack(character.getAttack() + 2);
+                }if(character.characterType.equals("highLander")){
+                    character.setSpeed(character.getspeed() - 1);
+                    character.setDefence(character.getDefence() - 1);
+                }if(character.characterType.equals("marshLander")) {
+                    character.setSpeed(character.getspeed() - 1);
+                    character.setDefence(character.getDefence() - 1);
+                }
+
+            }
+
+        }
+        challengerArmy.sort(Comparator.comparingDouble(character -> character.getSpeed()));
+        opponentArmy.sort(Comparator.comparingDouble(character -> character.getSpeed()));
+
+        while (!isWarEnded) {
+
+        }
+
 
 
 
