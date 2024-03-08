e2768b70a41cc7b791c748e7cfb70c2a3ba3df2f
trinith01 <gt202054104@gmail.com>
2024-03-08 17:16:09 +0530

    Edited War Class

--- OOP-Project/War.java
+++ OOP-Project/War.java
@@ -26,50 +26,67 @@ public class War {
         this.challengerArmy = challenger.getArmy();
         this.opponentArmy= opponent.getArmy();
         for(Character character:challengerArmy){
-            if(challenger.getHomeGround().equals("hillCrest")) {
-                if (character.getCharacterType().equals("highLander")) {
-                    character.setAttack(character.getAttack() + 1);
-                    character.setDefence(character.getDefence() + 1);
+            switch (challenger.getHomeGround()) {
+                case "hillCrest" -> {
+                    if (character.getCharacterType().equals("highLander")) {
+                        character.setAttack(character.getAttack() + 1);
+                        character.setDefence(character.getDefence() + 1);
+                    }
                 }
-            }else if(challenger.getHomeGround().equals("marshLand")) {
-                if (character.getCharacterType().equals("marshLander")) {
-                    character.setDefence(character.getDefence() + 2);
-                }
-                if (character.getCharacterType().equals("sunChild")) {
-                    character.setAttack(character.getAttack() - 1);
+                case "marshLand" -> {
+                    if (character.getCharacterType().equals("marshLander")) {
+                        character.setDefence(character.getDefence() + 2);
+                    }
+                    if (character.getCharacterType().equals("sunChild")) {
+                        character.setAttack(character.getAttack() - 1);
 
-                }
-                if (character.getCharacterType().equals("mystics")) {
-                    character.setSpeed(character.getSpeed() - 1);
+                    }
+                    if (character.getCharacterType().equals("mystics")) {
+                        character.setSpeed(character.getSpeed() - 1);
 
+                    }
                 }
-            }else if(challenger.getHomeGround().equals("desert")){
-                    if(character.getCharacterType().equals("marshLander")){
+                case "desert" -> {
+                    if (character.getCharacterType().equals("marshLander")) {
                         character.setHealth(character.getHealth() - 1);
-                    }if(character.getCharacterType().equals("sunChild")){
+                    }
+                    if (character.getCharacterType().equals("sunChild")) {
                         character.setAttack(character.getAttack() + 1);
 
                     }
-
-            }else if(challenger.getHomeGround().equals("arcane")){
-                if(character.getCharacterType().equals("mystics")){
-                    character.setAttack(character.getAttack() + 2);
-                }if(character.getCharacterType().equals("highLander")){
-                    character.setSpeed(character.getSpeed() - 1);
-                    character.setDefence(character.getDefence() - 1);
-                }if(character.getCharacterType().equals("marshLander")) {
-                    character.setSpeed(character.getSpeed() - 1);
-                    character.setDefence(character.getDefence() - 1);
                 }
-
+                case "arcane" -> {
+                    if (character.getCharacterType().equals("mystics")) {
+                        character.setAttack(character.getAttack() + 2);
+                    }
+                    if (character.getCharacterType().equals("highLander")) {
+                        character.setSpeed(character.getSpeed() - 1);
+                        character.setDefence(character.getDefence() - 1);
+                    }
+                    if (character.getCharacterType().equals("marshLander")) {
+                        character.setSpeed(character.getSpeed() - 1);
+                        character.setDefence(character.getDefence() - 1);
+                    }
+                }
             }
 
         }
-        challengerArmy.sort(Comparator.comparingDouble(character -> character.getSpeed()));
-        opponentArmy.sort(Comparator.comparingDouble(character -> character.getSpeed()));
 
+        Vector<Character> challengerArmyAttackingArray = challengerArmy;
+        Vector<Character> opponentArmyAttackingArray = opponentArmy;
+        Vector<Character> challengerArmyDefendingArray = challengerArmy;
+        Vector<Character> opponentArmyDefendingArray = opponentArmy;
+
+        challengerArmyAttackingArray.sort(Comparator.comparingDouble(Character::getSpeed));
+        opponentArmyAttackingArray.sort(Comparator.comparingDouble(Character::getSpeed));
+        challengerArmyDefendingArray.sort(Comparator.comparingDouble(Character::getDefence));
+        opponentArmyDefendingArray.sort(Comparator.comparingDouble(Character::getDefence));
+
+        int attackingSide = 0; //set to 0 when challenger is attacking and set to 1 when opponent is attacking
         while (!isWarEnded) {
-
+            if (attackingSide == 0) {
+                opponentArmyDefendingArray.get(0).setHealth();
+            }
         }
 
 
