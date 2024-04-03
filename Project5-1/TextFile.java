9f0d1d52e44190fce95b26f2afbb10d5724c826b
vihanga_02 <muthumalavdw.22@uom.lk>
2024-04-03 18:34:58 +0530

    text file

--- Project5-1/TextFile.java
+++ Project5-1/TextFile.java
@@ -1,2 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TextFile {
+    String content;
+    String fileType;
+
+    StringBuilder contentBuilder = new StringBuilder();
+    public TextFile(String filePath) {
+        this.readFromFile(filePath);
+    }
+    public void readFromFile (String filePath){
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                contentBuilder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred while reading the file: " + e.getMessage());
+        }
+        finally {
+            this.content = contentBuilder.toString();
+        }
+    }
+
+    public void getFileType(String fileType){
+
+    }
+
 }
