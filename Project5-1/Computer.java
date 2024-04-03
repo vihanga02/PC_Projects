6d92a20e429aeb9a02b90f46f4e0ffc42d5e272e
Rivindu Thareendra <ugrtk2002@gmail.com>
2024-04-03 17:17:52 +0530

    Computer class

--- Project5-1/Computer.java
+++ Project5-1/Computer.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Computer {
+    private String computerName;
+
+    public static ArrayList<String> filetypes =new ArrayList<>();
+
+
+    public Computer(String computerName) {
+        this.computerName = computerName;
+    }
+    public String getComputerName() {
+        return computerName;
+    }
+    public void setComputerName(String computerName) {
+        this.computerName = computerName;
+    }
+    public void createPrintJob(String fileId,String fileName,String fileType,Byte[] content ) throws TypeNotSupportedException{
+        if(filetypes.contains(fileType)){
+            new PrintJob(fileId,fileName,fileType,content);
+        }else{
+            throw new TypeNotSupportedException("File type not supported: " + fileType);
+        }
+
+
+    }
+
 }
