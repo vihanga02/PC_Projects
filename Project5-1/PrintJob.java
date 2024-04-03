3544b58980a464548141057162f9b4af536fe234
vihanga_02 <muthumalavdw.22@uom.lk>
2024-04-03 16:02:50 +0530

    printJob getters and setters

--- Project5-1/PrintJob.java
+++ Project5-1/PrintJob.java
@@ -10,4 +10,36 @@ public class PrintJob {
         this.fileType = fileType;
         this.content = content;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public Byte[] getContent() {
+        return content;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setContent(Byte[] content) {
+        this.content = content;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
 }
