7b810d00de5426c78a8b93631d876a478cf91af3
vihanga_02 <muthumalavdw.22@uom.lk>
2024-04-03 15:57:21 +0530

    printJob constructor

--- Project5-1/PrintJob.java
+++ Project5-1/PrintJob.java
@@ -1,2 +1,13 @@
 public class PrintJob {
+    private String id;
+    private String fileName;
+    private String fileType;
+    private Byte[] content;
+
+    public PrintJob(String id, String fileName, String fileType, Byte[] content){
+        this.id = id;
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.content = content;
+    }
 }
