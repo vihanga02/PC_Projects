4dbd9fe2c7442b2932bcd3c379bcf1cf46aa9c7c
Rivindu Thareendra <ugrtk2002@gmail.com>
2024-04-03 17:46:00 +0530

    Printer class

--- Project5-1/Printer.java
+++ Project5-1/Printer.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class Printer {
+    private String printerName;
+    private ArrayList<String> compatibleTypes;
+    private boolean printStatus;
+
+    public Printer(String printerName, ArrayList<String> compatibleTypes){
+        this.printerName = printerName;
+        this.compatibleTypes = compatibleTypes;
+    }
+
+    public boolean isPrinting() {
+        return printStatus;
+    }
+
+    public void setPrintStatus(boolean printStatus) {
+        this.printStatus = printStatus;
+    }
+
+    public void print(PrintJob printJob) throws TypeNotSupportedException {
+        if (compatibleTypes.contains(printJob.getFileType())) {
+            System.out.println("Printing ");
+            setPrintStatus(true);
+        } else {
+            throw new TypeNotSupportedException("File type not supported for printing");
+        }
+    }
+
 }
