2f8e6728354fa3bbb216bbb9368ac543f09105db
vihanga_02 <muthumalavdw.22@uom.lk>
2024-04-03 16:44:47 +0530

    queue done

--- Project5-1/SharedQueue.java
+++ Project5-1/SharedQueue.java
@@ -1,2 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SharedQueue {
+    private Queue<PrintJob> queue;
+    private static final int capacity = 5;
+    public SharedQueue(){
+        this.queue = new LinkedList<>();
+    }
+    public void enqueue(PrintJob job) {
+        if (queue.size() < capacity){
+            queue.add(job);
+            System.out.println("Job added to the queue.");
+        }
+        else{
+            System.out.println("Queue is full");
+        }
+    }
+
+    public void dequeue(PrintJob job) {
+        if (queue.isEmpty()) {
+            System.out.println("Queue is empty.");
+        }
+        else if (queue.contains(job)) {
+            System.out.println("This job is not in the queue.");
+        }
+        else{
+            queue.remove(job);
+        }
+    }
 }
