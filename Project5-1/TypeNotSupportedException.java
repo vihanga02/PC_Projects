411de2b1b69bef21727198ab498dcba8d5998d2b
vihanga_02 <muthumalavdw.22@uom.lk>
2024-04-03 15:33:08 +0530

    Exception class done

--- Project5-1/TypeNotSupportedException.java
+++ Project5-1/TypeNotSupportedException.java
@@ -1,3 +1,7 @@
-public class TypeNotSupportedException {
+import java.security.PublicKey;
 
+public class TypeNotSupportedException extends Exception{
+    public TypeNotSupportedException(String messege){
+        super(messege);
+    }
 }
