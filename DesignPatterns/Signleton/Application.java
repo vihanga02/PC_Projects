6299b0c34c09f87becc2ef85adee896a98edd586
vihanga_02 <muthumalavdw.22@uom.lk>
2024-05-17 08:30:08 +0530

    singleton

--- DesignPatterns/Signleton/Application.java
+++ DesignPatterns/Signleton/Application.java
@@ -1,4 +1,46 @@
 package Signleton;
 
-public class Application {
+// The Database class defines the `getInstance` method that lets
+// clients access the same instance of a database connection
+// throughout the program.
+class Database{
+    // the static field for store the instance of the class
+     private static Database instance;
+
+    // The singleton's constructor should always be private to
+    // prevent direct construction calls with the `new`
+    // operator.
+    private Database(){}
+
+    // The static method that controls access to the singleton
+    // instance.
+    public static Database getInstance(){
+         if (instance == null){
+             instance = new Database();
+         }
+         return instance;
+    }
+
+    public void query(String sql) {
+        // For instance, all database queries of an app go
+        // through this method. Therefore, you can place
+        // throttling or caching logic here.
+        // ...
+        System.out.println("Executing query: " + sql);
+    }
 }
+
+public class Application {
+    public static void main(String[] args) {
+        Database foo = Database.getInstance();
+        foo.query("SELECT * FROM users");
+
+        Database bar = Database.getInstance();
+        bar.query("SELECT * FROM products");
+
+        // The variable `bar` will contain the same object as
+        // the variable `foo`.
+        System.out.println("foo == bar: " + (foo == bar));
+    }
+}
+
