22da534d68d2d17683be55d461c1b634e16fbb9c
vihanga_02 <muthumalavdw.22@uom.lk>
2024-05-20 23:29:05 +0530

    PP1 second problem

--- PP/1/src/PopCornMachine.java
+++ PP/1/src/PopCornMachine.java
@@ -5,6 +5,15 @@ public class PopCornMachine {
     private static final int MAX_CAPACITY = 3;
     protected static final BlockingQueue<String> outputBin = new ArrayBlockingQueue<>(MAX_CAPACITY);
 
+    static class Bell{
+        public static void ring(){
+            System.out.println("Bell rings.");
+        }
+    }
 
-
+     static class Light{
+        public static void blink(){
+            System.out.println("Light blinks.");
+        }
+    }
 }
--- PP/1/src/RobotArm.java
+++ PP/1/src/RobotArm.java
@@ -3,6 +3,8 @@ public class RobotArm implements Runnable{
     public void pickup() throws InterruptedException {
         String popcornBag = PopCornMachine.outputBin.take();
         System.out.println("Robot arm picked: " + popcornBag);
+        PopCornMachine.Bell.ring();
+        PopCornMachine.Light.blink();
         Thread.sleep(1000);
     }
 
