48dec940229016eb0e488b7170db8fa2d0a36876
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-07-23 11:21:18 +0530

    Update ExpressionEvaluator1.java

--- DesignPatterns/Iterator/ExpressionEvaluator1.java
+++ DesignPatterns/Iterator/ExpressionEvaluator1.java
@@ -1,78 +1,101 @@
-package Iterator;
+// This is the Iterator class providing the abstract
+// notion of iterating through a collection.
+interface Iterator {
+ public boolean hasNext();
+ public Object next();
+}
+// This is the Container class providing an iterator to
+// sequence through its contents.
+interface Container {
+ public Iterator getIterator();
+}
 
-// This interface ensures that every expression has its
-// own interpreter
-interface Expression {
-    public boolean interpreter(String context);
+// This container class is a box of numbers
+class NumberBox implements Container {
+ private int [] data;
+ private int size;
+ private int count;
+ public NumberBox(int size) {
+  this.size = size;
+  data = new int[size];
+  count = 0;
 }
-// This is a leaf-level expression that evaluate values
-// and in this instance returns a boolean value
-class TerminalExpression implements Expression {
-    private String data;
-    public TerminalExpression(String data) { this.data = data; }
-    public boolean interpreter(String context) {
-        if(context.contains(data))
-            return true;
-        else
-            return false;
-    }
+ public boolean store(int val) {
+  if(count<size) {
+   data[count++] = val;
+   return true;
+  } else
+   return false;
+ }
+
+public class BoxIterator implements Iterator {
+  private int idx;
+  public BoxIterator() { idx = 0; }
+  public boolean hasNext() {
+   if(idx<count)
+    return true;
+   else
+    return false;
+  }
+  public Object next() { return data[idx++]; }
+ }
+ public Iterator getIterator() {
+  return new BoxIterator();
+} }
+
+// This container class is bucket of strings
+class StringBucket implements Container {
+ private String [] data;
+ private int size;
+ private int count;
+ public StringBucket(int size) {
+  this.size = size;
+  data = new String[size];
+  count = 0;
 }
-// This is a composite expression that evaluate other
-// expressions and specifically OR condition
-class ORExpression implements Expression {
-    private Expression left, right;
-    public ORExpression(Expression left, Expression right) {
-        this.left = left; this.right = right;
-    }
-    public boolean interpreter(String context) {
-        return
-                left.interpreter(context) || right.interpreter(context);
-    }
-}
-// This is a composite expression that evaluate other
-// expressions and specifically AND condition
-class ANDExpression implements Expression {
-    private Expression left, right;
-    public ANDExpression(Expression left, Expression right) {
-        this.left = left; this.right = right;
-    }
-    public boolean interpreter(String context) {
-        return
-                left.interpreter(context) && right.interpreter(context);
-    }
-}
-// This is a composite expression that evaluate another
-// expression and specifically NOT condition
-class NOTExpression implements Expression {
-    private Expression exp;
-    public NOTExpression(Expression exp) {
-        this.exp = exp;
-    }
-    public boolean interpreter(String context) {
-        return !exp.interpreter(context);
-    }
-}
-public class ExpressionEvaluator1 {
-    public static void main(String [] args) {
-// These are the symbols/terminal
-        Expression port1 = new TerminalExpression("HDMI");
-        Expression port2 = new TerminalExpression("VGA");
-        Expression adapter = new TerminalExpression("HDM2VG");
-// These are the syntax/grammar rules
-        Expression isVideoOutput1 = new ORExpression(port1,port2);
-        Expression isVideoOutput2 = new ANDExpression(port1,adapter);
-        Expression isVideoOutput3 = new NOTExpression(adapter);
-// we will check if given ports give a video output,
-// that is - evaluation/interpretation
-        System.out.println(isVideoOutput1.interpreter
-                ("Will I get a video output with HDMI"));
-        System.out.println(isVideoOutput1.interpreter("VGA"));
-        System.out.println(isVideoOutput1.interpreter("HDM2VG"));
-        System.out.println(isVideoOutput2.interpreter("HDMI,HDM2VG"));
-        System.out.println(isVideoOutput2.interpreter("VGA,HDM2VG"));
-        System.out.println(isVideoOutput2.interpreter("HDM2VG"));
-        System.out.println(isVideoOutput3.interpreter("HDMI"));
-        System.out.println(isVideoOutput3.interpreter("VGA"));
-        System.out.println(isVideoOutput3.interpreter("HDM2VG"));
-    }
+ public boolean store(String val) {
+  if(count<size) {
+   data[count++] = val;
+   return true;
+  } else
+   return false;
+ }
+
+public class BucketIterator implements Iterator {
+  private int idx;
+  public BucketIterator() { idx = 0; }
+  public boolean hasNext() {
+   if(idx<count)
+    return true;
+   else
+    return false;
+  }
+  public Object next() { return data[idx++]; }
+ }
+ public Iterator getIterator() {
+  return new BucketIterator();
+} }
+
+public class CollectionIterator {
+ public static void main(String [] args) {
+  // create a new box of numbers
+  NumberBox nbox = new NumberBox(10);
+  nbox.store(1); nbox.store(3); nbox.store(5);
+  nbox.store(6); nbox.store(8); nbox.store(10);
+  nbox.store(12); nbox.store(13);
+  // create an iterator of the box
+  Iterator iter = nbox.getIterator();
+  while(iter.hasNext())
+   System.out.println(iter.next());
+
+ // create a new bucket of strings
+  StringBucket sbin = new StringBucket(10);
+  sbin.store("apple"); sbin.store("hp"); sbin.store("asus");
+  sbin.store("dell"); sbin.store("acer"); sbin.store("lenovo");
+  sbin.store("ewis"); sbin.store("toshiba");
+  // use the same iterator for the bucket
+  iter = sbin.getIterator();
+  while(iter.hasNext())
+   System.out.println(iter.next());
+ }
 }
